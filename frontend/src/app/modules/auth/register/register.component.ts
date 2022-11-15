0ea704aa1e431815b587f115d51c1729c606057d
Milan Sekulic <milan.sekkulic@gmail.com>
2022-11-15 02:22:12 +0100

    Add back-arrow to registration component

--- frontend/src/app/modules/auth/register/register.component.ts
+++ frontend/src/app/modules/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  faChevronLeft = faChevronLeft;
 
   constructor() {
     document.getElementById('register-email')?.focus();
