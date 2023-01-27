e592baae684c61bf2e307f26d4a223a90b255c6f
Milan Sekulic <milan.sekkulic@gmail.com>
2023-01-27 05:52:22 +0100

    Implement endpoint for fetching driver reviews

--- frontend/src/app/modules/user/profile/driver/driver-profile/driver-reviews/driver-reviews.component.ts
+++ frontend/src/app/modules/user/profile/driver/driver-profile/driver-reviews/driver-reviews.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DriverService } from 'src/app/core/http/user/driver.service';
 import { Driver } from 'src/app/shared/models/driver.model';
+import { DriverReview } from 'src/app/shared/models/review.model';
 
 @Component({
   selector: 'app-driver-reviews',
@@ -7,10 +9,15 @@ import { Driver } from 'src/app/shared/models/driver.model';
 })
 export class DriverReviewsComponent implements OnInit {
   @Input() driver!: Driver;
+  reviews: DriverReview[] = [];
 
-  constructor() { }
+  constructor(private driverService: DriverService) { }
 
   ngOnInit(): void {
+    this.driverService.fetchReviews(this.driver.username, 0, 10)
+    .then((res) => {
+      console.log(res.data);
+    });
   }
 
 }
